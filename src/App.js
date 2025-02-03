faf9f65e4aafca17bfaebb26d1689c2069d15785
Tung Pham <tung.42@gmail.com>
2025-02-03 15:32:51 +0700

    Kambria

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import WordPuzzle from "./components/WordPuzzle";
+import WordPuzzleKambria from "./components/WordPuzzleKambria";
 
 const App = () => {
   return (
-    <Container className="my-5">
-      <WordPuzzle />
-    </Container>
+    <Router>
+      <Container className="my-5">
+        <Routes>
+          <Route path="/" element={<WordPuzzle />} />
+          <Route path="/kambria" element={<WordPuzzleKambria />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 };
 
