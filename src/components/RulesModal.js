079991de5169bfeb9a7fb3706553cd33ce6f9bff
Tung Pham <tung.42@gmail.com>
2025-02-06 14:55:38 +0700

    Update RulesModal.js

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -8,7 +8,7 @@ const ruleItems = [
   'Khi quay phải "Mất lượt", người chơi phải nhường lượt chơi cho người chơi kế tiếp.',
   'Nếu đoán đúng, ký tự sẽ hiển thị trong ô chữ và người chơi sẽ được số điểm tương ứng với số lần xuất hiện của ký tự. Ngoại trừ nếu quay phải "Mất điểm" hoặc "Mất lượt" thì sẽ không được điểm',
   "Nếu đoán sai, sẽ nhường lượt cho người chơi kế tiếp.",
-  "Nếu có nhiều điểm nhất sau khi ô chữ đã được đoán xong, người chơi THẮNG.",
+  "Người chơi nào có nhiều điểm nhất sau khi ô chữ đã được đoán xong sẽ THẮNG.",
   "Người chơi có thể chọn chơi lại sau khi kết thúc ván.",
 ];
 
