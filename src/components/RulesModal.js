c10df933ce9de14b7195b3bc1995117e0f21978a
Tung Pham <tung.42@gmail.com>
2025-02-06 14:52:42 +0700

    Update RulesModal.js

--- src/components/RulesModal.js
+++ src/components/RulesModal.js
@@ -2,13 +2,13 @@ import { Modal, Button } from "react-bootstrap";
 
 const ruleItems = [
   "Trò chơi hiển thị một ô chữ với các ô trống tương ứng với số ký tự trong từ/cụm từ bí mật.",
-  "Người chơi đoán từng chữ cái một.",
-  'Người chơi có thể quay phải ô 100, 200, 300,... đến 1000, đặc biệt có 2 ô "Mất điểm" và "Mất lượt".',
+  "Người chơi đoán từng ký tự một.",
+  'Người chơi có thể quay phải 100, 200, 300,... đến 1000, đặc biệt có 2 hình phạt "Mất điểm" và "Mất lượt".',
   'Khi quay phải "Mất điểm", người chơi vẫn còn lượt nhưng số điểm bị về 0.',
   'Khi quay phải "Mất lượt", người chơi phải nhường lượt chơi cho người chơi kế tiếp.',
-  'Nếu đoán đúng, chữ cái sẽ hiển thị trong ô chữ và người chơi sẽ được số điểm tương ứng với số lần xuất hiện của chữ cái. Ngoại trừ nếu quay phải "Mất điểm" hoặc "Mất lượt" thì sẽ không được điểm',
-  "Nếu đoán sai, sẽ đổi lượt cho người chơi kế tiếp.",
-  "Nếu có nhiều điểm nhất sau khi ô chữ đã được đoán xong, người chơi thắng.",
+  'Nếu đoán đúng, ký tự sẽ hiển thị trong ô chữ và người chơi sẽ được số điểm tương ứng với số lần xuất hiện của ký tự. Ngoại trừ nếu quay phải "Mất điểm" hoặc "Mất lượt" thì sẽ không được điểm',
+  "Nếu đoán sai, sẽ nhường lượt cho người chơi kế tiếp.",
+  "Nếu có nhiều điểm nhất sau khi ô chữ đã được đoán xong, người chơi THẮNG.",
   "Người chơi có thể chọn chơi lại sau khi kết thúc ván.",
 ];
 
